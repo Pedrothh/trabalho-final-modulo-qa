08aab1da83be72e6e65266009a235b0f67641db5
Pedro Thomé <pedro.th.dev@gmail.com>
2022-11-04 16:30:38 -0300

    ADD: Buscar Endereço test

---
+++ trabalho-final-modulo-qa/src/test/java/br/com/dbcompany/aceitacao/EnderecoAceitacaoTest.java
@@ -0,0 +1,58 @@
+package br.com.dbcompany.aceitacao;
+
+import br.com.dbcompany.dto.*;
+import br.com.dbcompany.service.*;
+import br.com.dbcompany.utils.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class EnderecoAceitacaoTest {
+
+    String jsonBody = lerJson("src/test/resources/data/user1.json");
+
+    String jsonBody2 = lerJson("src/test/resources/data/user2.json");
+
+    String jsonBodyEndereco = lerJson("src/test/resources/data/endereco1.json");
+
+    PessoaService pessoaService = new PessoaService();
+
+    EnderecoService enderecoService = new EnderecoService();
+
+    public EnderecoAceitacaoTest() throws IOException {
+    }
+
+    public String lerJson(String caminhoJson) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(caminhoJson)));
+    }
+
+
+    @Test
+    public void testDeveBuscarEndereco(){
+        UserPayloadDTO serviceResult = pessoaService.addPessoa(jsonBody);
+
+        ListaEnderecoDTO result = enderecoService.buscarEndereco();
+
+        Assert.assertNotNull(result);
+
+        pessoaService.deletePessoa(serviceResult.getIdPessoa());
+
+    }
+
+
+    // BUG NO QUERYPARAM ADD
+    @Test
+    public void testDeveBuscarEnderecoPorIdEndereco(){
+        UserPayloadDTO serviceResult = pessoaService.addPessoa(jsonBody);
+
+        UserEnderecoDTO serviceEndResult = enderecoService.addEndereco(serviceResult.getIdPessoa(), jsonBodyEndereco);
+
+        UserEnderecoDTO result = enderecoService.buscarEnderecoPorIdEndereco(serviceEndResult.getIdEndereco());
+
+        pessoaService.deletePessoa(serviceResult.getIdPessoa());
+
+    }
+}
