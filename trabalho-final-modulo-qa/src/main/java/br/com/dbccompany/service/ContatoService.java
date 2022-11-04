abd201d05b49f03b8aa0c79e2d01ddd6ba538d4b
Pedro Thomé <pedro.th.dev@gmail.com>
2022-11-04 13:49:22 -0300

    :ok_hand: Organiznado projeto

---
+++ trabalho-final-modulo-qa/src/main/java/br/com/dbccompany/service/ContatoService.java
@@ -0,0 +1,76 @@
+package br.com.dbccompany.service;
+
+import br.com.dbccompany.dto.ContatoDTO;
+import br.com.dbccompany.dto.ResponseUserDTO;
+import br.com.dbccompany.utils.Login;
+
+import static io.restassured.RestAssured.given;
+
+public class ContatoService {
+
+    String baseUri = "http://vemser-dbc.dbccompany.com.br:39000/vemser/dbc-pessoa-api";
+
+    String tokenAdm = new Login().autenticacaoAdmin();
+
+
+    /////////////////////////////////////////////////
+    ///////// METODOS DO CONTATO-CONTROLLER /////////
+    /////////////////////////////////////////////////
+
+    public ContatoDTO[] buscarContato(){
+
+        ContatoDTO[] result =
+                given()
+                        .log().all()
+                        .header("Authorization", tokenAdm)
+                        .when()
+                        .get(baseUri + "/contato")
+                        .then()
+                        .log().all()
+                        .statusCode(200)
+                        .extract().as(ContatoDTO[].class);
+
+        return result;
+    }
+
+    public ContatoDTO[] buscarContatoPeloIdPessoa(){
+        String idPessoa = "66";
+
+        ContatoDTO[] result =
+                given()
+                        .log().all()
+                        .header("Authorization", tokenAdm)
+                        .pathParams("idPessoa", idPessoa)
+                        .when()
+                        .get(baseUri + "/contato/{idPessoa}")
+                        .then()
+                        .log().all()
+                        .statusCode(200)
+                        .extract().as(ContatoDTO[].class);
+
+        return result;
+    }
+
+    public ContatoDTO criarContatoPeloIdPessoa() {
+        String idPessoa = "66";
+
+        ContatoDTO result =
+                given()
+                        .log().all()
+                        .header("Authorization", tokenAdm)
+                        .pathParams("idPessoa", idPessoa)
+                        .when()
+                        .post(baseUri + "/contato/{idPessoa}")
+                        .then()
+                        .log().all()
+                        .statusCode(200)
+                        .extract().as(ContatoDTO.class);
+
+        return result;
+
+    }
+
+    public ResponseUserDTO deletarContatoPeloIdContato() {
+
+    }
+}
--- trabalho-final-modulo-qa/src/test/java/br/com/dbccompany/aceitacao/PessoaAceitacaoTest.java
+++ trabalho-final-modulo-qa/src/test/java/br/com/dbccompany/aceitacao/PessoaAceitacaoTest.java
@@ -4,6 +4,7 @@ import br.com.dbccompany.dto.ContatoDTO;
 import br.com.dbccompany.dto.RelatorioDTO;
 import br.com.dbccompany.dto.UserDTO;
 import br.com.dbccompany.dto.UserPayloadDTO;
+import br.com.dbccompany.service.ContatoService;
 import br.com.dbccompany.service.PessoaService;
 import io.restassured.response.Response;
 
@@ -19,7 +20,13 @@ import static io.restassured.RestAssured.*;
 
 public class PessoaAceitacaoTest {
     String jsonBody = lerJson("src/test/resources/data/user1.json");
-    PessoaService service = new PessoaService();
+
+    String jsonBodyContato = lerJson("src/test/resources/data/contato1user1.json");
+    PessoaService pessoaService = new PessoaService();
+
+
+
+
         public String lerJson(String caminhoJson) throws IOException {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
@@ -27,71 +34,51 @@ public class PessoaAceitacaoTest {
     public PessoaAceitacaoTest() throws IOException {
     }
 
+
+    ////////////////////////////////////////////////
+    ///////// TESTES DO PESSOA-CONTROLLER /////////
+    ////////////////////////////////////////////////
+    @Test
+    public void testDeveAdicionarPessoaComSucesso()  {
+        UserPayloadDTO serviceResult = pessoaService.addPessoa(jsonBody);
+
+        Assert.assertEquals(serviceResult.getNome(), "Mordekaiser");
+        Assert.assertEquals(serviceResult.getCpf(), "56448824325");
+
+        pessoaService.deletePessoa(serviceResult.getId());
+    }
+
+    @Test
+    public void testDeveDeletarPessoaComSucesso()  {
+        UserPayloadDTO serviceResult = pessoaService.addPessoa(jsonBody);
+        
+        Response response = pessoaService.deletePessoa(serviceResult.getId());
+        
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+
+
+
     @Test
     public void testeDeveRetornarRelatorioPessoas(){
 
-        RelatorioDTO[] resultService = service.buscarRelatorio();
+        RelatorioDTO[] resultService = pessoaService.buscarRelatorio();
 
         Assert.assertEquals(resultService[0].getNomePessoa().toUpperCase(), "Maicon Machado Gerardi".toUpperCase());
 
 
     }
 
-    ////////////////////////////////////////////////
-    ///////// TESTES DO CONTATO-CONTROLLER /////////
-    ////////////////////////////////////////////////
-    @Test
-    public void testeDeveBuscarContato(){
-
-        ContatoDTO[] resultService = service.buscarContato();
-
-        Assert.assertEquals(resultService[0].getIdPessoa(), "1");
-        Assert.assertEquals(resultService[0].getTipoContato(), "COMERCIAL");
-        Assert.assertEquals(resultService[0].getTelefone(), "51955565585");
-        Assert.assertEquals(resultService[0].getDescricao(), "whatsapp");
-        Assert.assertEquals(resultService[0].getIdContato(), "1");
-    }
 
 
     ////////////////////////////////////////////////
-    ///////// TESTES DO PESSOA-CONTROLLER /////////
+    ///////// TESTES DO AUTH-CONTROLLER /////////
     ////////////////////////////////////////////////
-    @Test
-    public void testDeveAdicionarPessoaComSucesso() throws Exception {
-        UserPayloadDTO serviceResult = service.addPessoa(jsonBody);
-
-        Assert.assertEquals(serviceResult.getNome(), "Mordekaiser");
-        Assert.assertEquals(serviceResult.getCpf(), "56448824325");
-
-        service.deletePessoa(serviceResult.getId());
-    }
-
-    @Test
-    public void testDeveDeletarPessoaComSucesso() throws Exception { 
-        UserPayloadDTO serviceResult = service.addPessoa(jsonBody);
-        
-        Response response = service.deletePessoa(serviceResult.getId());
-        
-        Assert.assertEquals(response.getStatusCode(), 200);
-    }
-
-
-    @Test
-    public void testeDeveBuscarContatoPeloIdPessoa() {
-
-        ContatoDTO[] resultService = service.buscarContatoPeloIdPessoa();
-
-        Assert.assertEquals(resultService[0].getIdPessoa(), "1");
-        Assert.assertEquals(resultService[0].getTipoContato(), "COMERCIAL");
-        Assert.assertEquals(resultService[0].getTelefone(), "51955565585");
-        Assert.assertEquals(resultService[0].getDescricao(), "whatsapp");
-        Assert.assertEquals(resultService[0].getIdContato(), "1");
-    }
 
     @Test
     public void testeDeveCriarNovoUserAdmin(){
 
-        UserDTO resultService = service.criarUsuarioAdmin();
+        UserDTO resultService = pessoaService.criarUsuarioAdmin();
 
         Assert.assertEquals(resultService.getLogin(), "alainpedrotestuser");
         Assert.assertNotNull(resultService.getIdUsuario());
