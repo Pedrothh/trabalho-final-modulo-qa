738e9b02e9f598af52f51999a89e6755e213196f
Pedro Thomé <pedro.th.dev@gmail.com>
2022-11-05 04:01:34 -0300

    :heavy_check_mark: TEST: Finalizando os cenários positivos de teste de contatos

--- trabalho-final-modulo-qa/src/main/java/br/com/dbcompany/service/ContatoService.java
+++ trabalho-final-modulo-qa/src/main/java/br/com/dbcompany/service/ContatoService.java
@@ -3,6 +3,7 @@ package br.com.dbcompany.service;
 import br.com.dbcompany.dto.*;
 import br.com.dbcompany.utils.*;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.given;
 
@@ -19,54 +20,72 @@ public class ContatoService {
 
     public ContatoDTO[] buscarContato(){
 
-        ContatoDTO[] result =
-                given()
-                        .log().all()
-                        .header("Authorization", tokenAdm)
-                        .when()
-                        .get(baseUri + "/contato")
-                        .then()
-                        .log().all()
-                        .statusCode(200)
-                        .extract().as(ContatoDTO[].class);
-
-        return result;
+        return given()
+                .log().all()
+                .header("Authorization", tokenAdm)
+                .when()
+                .get(baseUri + "/contato")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract().as(ContatoDTO[].class);
     }
 
     public ContatoDTO[] buscarContatoPeloIdPessoa(Integer idPessoa){
-        ContatoDTO[] result =
-                given()
-                        .log().all()
-                        .header("Authorization", tokenAdm)
-                        .pathParams("idPessoa", idPessoa)
-                        .when()
-                        .get(baseUri + "/contato/{idPessoa}")
-                        .then()
-                        .log().all()
-                        .statusCode(200)
-                        .extract().as(ContatoDTO[].class);
 
-        return result;
+        return given()
+                .log().all()
+                .header("Authorization", tokenAdm)
+                .pathParams("idPessoa", idPessoa)
+                .when()
+                .get(baseUri + "/contato/{idPessoa}")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract().as(ContatoDTO[].class);
     }
 
     public ContatoDTO criarContatoPeloIdPessoa(Integer idPessoa, String requestBody) {
 
-        ContatoDTO result =
-                given()
-                        .log().all()
-                        .header("Authorization", tokenAdm)
-                        .pathParams("idPessoa", idPessoa)
-                        .contentType(ContentType.JSON)
-                        .body(requestBody)
-                        .when()
-                        .post(baseUri + "/contato/{idPessoa}")
-                        .then()
-                        .log().all()
-                        .statusCode(200)
-                        .extract().as(ContatoDTO.class);
-
-        return result;
-
+        return given()
+                .log().all()
+                .header("Authorization", tokenAdm)
+                .pathParams("idPessoa", idPessoa)
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post(baseUri + "/contato/{idPessoa}")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract().as(ContatoDTO.class);
     }
 
+    public ContatoDTO atualizarContatoPeloIdContato (Integer idContato, String requestBody) {
+        return given()
+                .log().all()
+                .header("Authorization", tokenAdm)
+                .pathParams("idContato", idContato)
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .put(baseUri + "/contato/{idContato}")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract().as(ContatoDTO.class);
+    }
+
+    public Response deletarContatoPeloIdContato (Integer idContato) {
+        return given()
+                .log().all()
+                .header("Authorization", tokenAdm)
+                .pathParams("idContato", idContato)
+                .when()
+                .delete(baseUri + "/contato/{idContato}")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract().response();
+    }
 }
--- trabalho-final-modulo-qa/src/test/java/br/com/dbcompany/aceitacao/ContatoAceitacaoTest.java
+++ trabalho-final-modulo-qa/src/test/java/br/com/dbcompany/aceitacao/ContatoAceitacaoTest.java
@@ -3,6 +3,8 @@ package br.com.dbcompany.aceitacao;
 import br.com.dbcompany.dto.ContatoDTO;
 import br.com.dbcompany.dto.UserPayloadDTO;
 import br.com.dbcompany.service.ContatoService;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,7 +13,8 @@ import java.io.IOException;
 
 public class ContatoAceitacaoTest extends PessoaAceitacaoTest {
 
-   String jsonBodyContato = lerJson("src/test/resources/data/contato1.json");
+    String jsonBodyContato = lerJson("src/test/resources/data/contato1.json");
+    String jsonBodyContato2 = lerJson("src/test/resources/data/contato2.json");
     ContatoService contatoService = new ContatoService();
 
     public ContatoAceitacaoTest() throws IOException {
@@ -66,11 +69,45 @@ public class ContatoAceitacaoTest extends PessoaAceitacaoTest {
 
         UserPayloadDTO serviceAddPessoa = service.addPessoa(jsonBody);
 
-        ContatoDTO resultService = contatoService.criarContatoPeloIdPessoa(serviceAddPessoa.getIdPessoa(), jsonBodyContato);
+        ContatoDTO resultAddContato = contatoService.criarContatoPeloIdPessoa(serviceAddPessoa.getIdPessoa(), jsonBodyContato);
 
-        Assert.assertNotNull(resultService.getIdContato());
+        Assert.assertNotNull(resultAddContato.getIdContato());
+        Assert.assertEquals(resultAddContato.getDescricao(), "whatsapp");
+        Assert.assertEquals(resultAddContato.getTelefone(), "(83)98383-8383");
+        Assert.assertEquals(resultAddContato.getTipoContato(), "RESIDENCIAL");
 
         service.deletePessoa(serviceAddPessoa.getIdPessoa());
+    }
+
+    @Test
+    public void testeDeveAtualizarContatoPorIdContato() {
+
+        UserPayloadDTO serviceAddPessoa = service.addPessoa(jsonBody);
+
+        ContatoDTO resultAddContato = contatoService.criarContatoPeloIdPessoa(serviceAddPessoa.getIdPessoa(), jsonBodyContato);
+
+        ContatoDTO resultAtualizarContato = contatoService.atualizarContatoPeloIdContato(resultAddContato.getIdContato(), jsonBodyContato2);
+
+        resultAtualizarContato.setIdPessoa(serviceAddPessoa.getIdPessoa());
+
+        Assert.assertEquals(resultAtualizarContato.getTelefone(), "(83)91111-1111");
+
+        service.deletePessoa(serviceAddPessoa.getIdPessoa());
+
+    }
+
+    @Test
+    public  void testeDeveDeletarContatoPorIdContato(){
+        UserPayloadDTO serviceAddPessoa = service.addPessoa(jsonBody);
+
+        ContatoDTO resultAddContato = contatoService.criarContatoPeloIdPessoa(serviceAddPessoa.getIdPessoa(), jsonBodyContato);
+
+        Response resultDeletarContato = contatoService.deletarContatoPeloIdContato(resultAddContato.getIdContato());
+
+        Assert.assertEquals(resultDeletarContato.getStatusCode(), 200);
+
+        service.deletePessoa(serviceAddPessoa.getIdPessoa());
+
     }
 
 
